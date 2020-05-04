85d0c971de847db6230a75320789995068e666ff
gagan_gupta <gagan@adyogi.com>
2020-05-04 20:51:47 +0530

    response headers

--- src/main/java/com/uem/controller/MainController.java
+++ src/main/java/com/uem/controller/MainController.java
@@ -41,11 +41,11 @@ public class MainController {
         CustomResponse customResponse =  mainService.signUp(email);
         if (customResponse.getSuccess()){
             return ResponseEntity.ok()
-                    .header("key", "value")
+                    .header("message", customResponse.getMessage())
                     .body(customResponse.getInfoAsJson().toString());
         }else {
             return ResponseEntity.badRequest()
-                    .header("key", "value")
+                    .header("message", customResponse.getMessage())
                     .body(customResponse.getMessage());
         }
     }
@@ -61,11 +61,11 @@ public class MainController {
         CustomResponse customResponse =  mainService.signIN(email, password, loginType.toUpperCase());
         if (customResponse.getSuccess()){
             return ResponseEntity.ok()
-                    .header("key", "value")
+                    .header("message", customResponse.getMessage())
                     .body(customResponse.getMessage());
         }else {
             return ResponseEntity.badRequest()
-                    .header("key", "value")
+                    .header("message", customResponse.getMessage())
                     .body(customResponse.getMessage());
         }
     }
@@ -102,11 +102,11 @@ public class MainController {
         List<User> users = mainService.getUserInfo(userID);
         if (users == null || users.size() == 0){
             return ResponseEntity.badRequest()
-                    .header("key", "value")
+                    .header("message", "")
                     .body(Constants.FAILURE);
         }else {
             return ResponseEntity.ok()
-                    .header("key", "value")
+                    .header("message", "")
                     .body(users.toString());
         }
     }
@@ -119,11 +119,11 @@ public class MainController {
         CustomResponse customResponse =  mainService.createUniversity(jsonObject);
         if (customResponse.getSuccess()){
             return ResponseEntity.ok()
-                    .header("key", "value")
+                    .header("message", customResponse.getMessage())
                     .body(customResponse.getInfo().toString());
         }else {
             return ResponseEntity.badRequest()
-                    .header("key", "value")
+                    .header("message", customResponse.getMessage())
                     .body(customResponse.getMessage());
         }
     }
